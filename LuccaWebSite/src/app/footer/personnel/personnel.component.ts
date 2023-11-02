1e3b5c48f4c47a766f26acb0f198e84d58400e3e
MBAYE-Mustafaa <mmbaye2905@gmail.com>
2023-11-02 18:21:28 +0100

    [Footer-Personnel] J'arrive pas à importer correctement les APIs. Apparemment il faut une autorisation CORS (après inspection du console) à voir avec Lucca

--- LuccaWebSite/src/app/footer/personnel/personnel.component.ts
+++ LuccaWebSite/src/app/footer/personnel/personnel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-personnel',
@@ -7,18 +6,82 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./personnel.component.scss']
 })
 export class PersonnelComponent implements OnInit {
-  personnes: any[] = [];
-
-  constructor(private http: HttpClient) {}
+  numPresentToday: number = 27; // Initialise la variable pour afficher le nombre de personnes présentes
 
   ngOnInit() {
-    const apiKey = '92ce5b56-2f13-4c8a-ade8-f6a3fc2fb70d';
-    const apiUrl = 'https://example.ilucca.net';
+    // Planifie l'exécution de la fonction de vérification chaque jour à 8h (en millisecondes)
+    const dailyCheckTime = 8 * 60 * 60 * 1000; // 8 heures en millisecondes
 
-    this.http.get(apiUrl, {
-      headers: { 'Authorization': `Bearer ${apiKey}` }
-    }).subscribe((data: any) => {
-      this.personnes = data; // En supposant que les données sont dans un tableau 'personnes'
+    setInterval(() => {
+      this.checkStatus(); // Appelle la fonction de vérification à intervalles réguliers
+    }, dailyCheckTime);
+
+    // Appelle la vérification immédiatement lors du chargement de la page
+    this.checkStatus();
+  }
+
+  checkStatus() {
+    this.getUsersCount(); // Fonction pour obtenir le nombre total d'utilisateurs
+  }
+
+  getUsersCount() {
+    // Requête API pour récupérer le nombre total d'utilisateurs
+    const usersApiUrl = 'https://example.ilucca.net/api/v3/users';
+    const headers = new Headers({
+      'Authorization': 'lucca application = ea3a8387-cf2f-45da-a169-26b45db9a18e', // Clé de l'API
     });
+
+    fetch(usersApiUrl, { method: 'GET', headers })
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Erreur lors de la récupération des utilisateurs');
+        }
+      })
+      .then(data => {
+        const totalUsers = data.data.length;
+
+        // Une fois que vous avez le nombre total d'utilisateurs, vous pouvez vérifier le nombre d'absents
+        this.getAbsentsCount(totalUsers);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  getAbsentsCount(totalUsers: number) {
+    // Requête API pour récupérer le nombre d'absents
+    const absencesApiUrl = 'https://example.ilucca.net/api/v3/leaves';
+    const date = 'today'; // 'today' pour vérifier les absences de la journée
+    const headers = new Headers({
+      'Authorization': 'lucca application = 92ce5b56-2f13-4c8a-ade8-f6a3fc2fb70d', // Clé de l'API
+    });
+
+    const params = new URLSearchParams();
+    params.append('leavePeriod.ownerId', 'all');
+    params.append('date', date);
+
+    const url = new URL(absencesApiUrl);
+    url.search = params.toString();
+
+    fetch(url, { method: 'GET', headers })
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Erreur lors de la récupération des absences');
+        }
+      })
+      .then(data => {
+        const absents = data.data.length;//obtenir le nombre d'absents
+
+        // Nous avons maintenant le nombre total d'utilisateurs et le nombre d'absents
+        // Nous pouvons calculer le nombre de personnes présentes
+        this.numPresentToday = totalUsers - absents;
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 }
