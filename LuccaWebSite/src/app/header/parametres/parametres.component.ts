1eee8dc17cdc84d6bbd6263567905572e5c50376
noikitu <adamferhout@gmail.com>
2023-11-09 11:34:24 +0100

    Parametres et sharedService

--- LuccaWebSite/src/app/header/parametres/parametres.component.ts
+++ LuccaWebSite/src/app/header/parametres/parametres.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { SharedService } from 'src/app/services/shared.service';
 @Component({
   selector: 'app-parametres',
   templateUrl: './parametres.component.html',
   styleUrls: ['./parametres.component.scss']
 })
 export class ParametresComponent {
+  showSettingsMenu = false;
+  selectedSiteLocation = 'Nantes';
+  miseajour = 0;
+  selectedLanguage = 'Fr';
+
+  constructor(private sharedService: SharedService) {
+    this.selectedSiteLocation = 'Nantes';
+  }
+
+  onOptionSelected(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    // Réagir à la sélection de l'option ici
+    this.handleOptionSelection(selectedValue);
+  }
+
+  handleOptionSelection(selectedValue: string) {
+    this.miseajour+=1;
+    this.sharedService.setLieu(selectedValue);
+  }
+
+  toggleSettingsMenu() {
+    this.showSettingsMenu = !this.showSettingsMenu;
+  }
+
+  public getLocation(){
+    return this.selectedSiteLocation;
+  }
 
 }
---
+++ LuccaWebSite/src/app/services/shared.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  private lieuSubject = new BehaviorSubject<string>('Nantes');
+  lieu$ = this.lieuSubject.asObservable();
+
+  setLieu(lieu: string) {
+    this.lieuSubject.next(lieu);
+  }
+}
