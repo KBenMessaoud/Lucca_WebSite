749d125ae73a64bc9240cd55ff73d0aac31b7b54
KBenMessaoud <karim.bmessaoud@gmail.com>
2023-12-07 10:06:41 +0100

    flex test

---
+++ LuccaWebSite/proxy.conf.mjs
@@ -0,0 +1,22 @@
+import { exec } from 'child_process';
+
+console.log('starting server ...');
+exec('node server/index.js', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+
+  console.log(`stdout:\n${stdout}`);
+});
+
+export default {
+  '/api/': {
+    target: 'http://localhost:3000',
+  }
+}
