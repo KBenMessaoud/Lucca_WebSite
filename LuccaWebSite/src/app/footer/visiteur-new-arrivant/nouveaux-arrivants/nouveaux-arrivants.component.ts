192df78a99b8f41c5c9fdc1662bea107f16aab23
KBenMessaoud <karim.bmessaoud@gmail.com>
2023-12-12 16:46:43 +0100

    Remise en page new arriv

--- LuccaWebSite/src/app/footer/visiteur-new-arrivant/nouveaux-arrivants/nouveaux-arrivants.component.ts
+++ LuccaWebSite/src/app/footer/visiteur-new-arrivant/nouveaux-arrivants/nouveaux-arrivants.component.ts
@@ -2,39 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { LuccaApiService } from '../../../services/lucca-api.service';
 
 @Component({
-  selector: 'app-nouveaux-arrivants',
-  templateUrl: './nouveaux-arrivants.component.html',
-  styleUrls: ['./nouveaux-arrivants.component.scss']
+  selector: 'app-nouveaux-arrivants',
+  templateUrl: './nouveaux-arrivants.component.html',
+  styleUrls: ['./nouveaux-arrivants.component.scss']
 })
 export class NouveauxArrivantsComponent implements OnInit {
-  nouveauxArrivants: any[] = [];
-  currentSlideIndex = 0;
+  nouveauxArrivants: any[] = [];
+  currentSlideIndex = 0;
 
-  constructor(private luccaApiService: LuccaApiService) { }
+  constructor(private luccaApiService: LuccaApiService) { }
 
-  ngOnInit(): void {
-    this.loadNouveauxArrivants();
-    // Changez la durée si nécessaire
-    setInterval(() => this.goToNextSlide(), 3000);
-  }
+  ngOnInit(): void {
+    this.loadNouveauxArrivants();
+    // Changez la durée si nécessaire
+    setInterval(() => this.goToNextSlide(), 3000);
+  }
 
-  loadNouveauxArrivants(): void {
-    this.luccaApiService.getUsers().subscribe({
-      next: (response) => {
-        // On s'assure que 'items' est bien présent dans 'data'
-        if (response && response.data && response.data.items) {
-          // On extrait le tableau 'items' de la réponse de l'API
-          this.nouveauxArrivants = response.data.items;
-        }
-      },
-      error: (error) => {
-        console.error('Erreur lors de la récupération des visiteurs:', error);
-      }
-    });
-  }
-  goToNextSlide(): void {
-    if (this.nouveauxArrivants && this.nouveauxArrivants.length > 0) {
-      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.nouveauxArrivants.length;
-    }
-  }
+  loadNouveauxArrivants(): void {
+    this.luccaApiService.getUsers().subscribe({
+      next: (response) => {
+        // On s'assure que 'items' est bien présent dans 'data'
+        if (response && response.data && response.data.items) {
+          // On extrait le tableau 'items' de la réponse de l'API
+          this.nouveauxArrivants = response.data.items;
+          // On limite le tableau à 5 éléments
+          this.nouveauxArrivants = this.nouveauxArrivants.slice(0, 5);
+        }
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération des visiteurs:', error);
+      }
+    });
+  }
+  goToNextSlide(): void {
+    if (this.nouveauxArrivants && this.nouveauxArrivants.length > 0) {
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.nouveauxArrivants.length;
+    }
+  }
 }
